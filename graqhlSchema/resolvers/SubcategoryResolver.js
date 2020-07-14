f01b26ab1fcb32e08a67981c68d1d69153334fbe
Daniel Walter <realdanielwalter@gmail.com>
2020-07-14 07:17:11 -0700

    I modified subcategory class

--- graqhlSchema/resolvers/SubcategoryResolver.js
+++ graqhlSchema/resolvers/SubcategoryResolver.js
@@ -1,6 +1,6 @@
 
 
-const CreateSubCategory = require('../../Controllers/subcategories/action/CreateSubcategory');
+const CreateSubCategory = require('../../Controllers/subcategories/action/createSubcategory');
 const ActivateSubcategory = require('../../Controllers/subcategories/action/ActivateSubcategory');
 
 module.exports = {
