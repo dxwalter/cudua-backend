39530a92e9ad3f7db8beeed5286a51bbb0d8d1d4
Daniel Walter <realdanielwalter@gmail.com>
2020-09-01 08:37:42 -0700

    i removed console from edit image

--- graqhlSchema/resolvers/BusinessResolver.js
+++ graqhlSchema/resolvers/BusinessResolver.js
@@ -98,7 +98,6 @@ module.exports = {
             const { filename, mimetype, createReadStream } = await args.input.file;
 
             let uploadLogo = new EditBusinessProfile();
-            console.log(filename + "here")
             return uploadLogo.changeBusinessLogo(args.input.businessId, args.input.file, userId);
            
         },
