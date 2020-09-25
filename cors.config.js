36772dde3c531c7e3146fa69db51f1c2e1078ae6
Daniel Walter <realdanielwalter@gmail.com>
2020-09-25 03:21:42 -0700

    i changed headers setting

--- cors.config.js
+++ cors.config.js
@@ -2,9 +2,9 @@ let whitelist = ['http://localhost:3333', 'https://cudua-ui.herokuapp.com'];
 let corsOptionsDelegate = function (req, callback) {
     let corsOptions;
     if (whitelist.indexOf(req.header('Origin')) !== -1) {
-      corsOptions = { origin: true, "methods": "GET,HEAD,PUT,PATCH,POST,DELETE", } // reflect (enable) the requested origin in the CORS response
+      corsOptions = { origin: true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE", } // reflect (enable) the requested origin in the CORS response
     } else {
-      corsOptions = { origin: false } // disable CORS for this request
+      corsOptions = { origin: false, methods: "GET,HEAD,PUT,PATCH,POST,DELETE" } // disable CORS for this request
     }
     console.log(corsOptions)
     callback(null, corsOptions) // callback expects two parameters: error and options
